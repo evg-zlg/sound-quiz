807760e11222ff181e5fc2488da18d787e4b308e
evg-zlg <evg.zalogin@gmail.com>
2022-11-05 01:43:02 +0500

    feat: add main section with welcom text and picture

--- songbird/src/pages/start-page/start-page.js
+++ songbird/src/pages/start-page/start-page.js
@@ -1,13 +1,13 @@
-import createHeader from "../../components/header/header.js";
+// import createHeader from "../../components/header/header.js";
 
-export default function createStartPage() {
-  let page = document.createElement("div");
-  let h1 = document.createElement("h1");
-  page.className = "content mx-auto max-w-[1080px] px-5";
-  h1.className = "title text-5xl my-10";
-  h1.innerText = "Welcome to sound quiz"
-  page.append(createHeader());
-  page.append(h1);
+// export default function createStartPage() {
+//   let page = document.createElement("div");
+//   let h1 = document.createElement("h1");
+//   page.className = "content mx-auto max-w-[1080px] px-5 ";
+//   h1.className = "title text-5xl my-10";
+//   h1.innerText = "Welcome to sound quiz"
+//   page.append(createHeader());
+//   page.append(h1);
 
-  return page;
-}
+//   return page;
+// }
--- songbird/webpack.config.js
+++ songbird/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
         loader: 'babel-loader',
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg|svg|)$/i,
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp)$/i,
         type: "asset/resource",
         generator: {
           filename: "images/[hash][ext][query]"
