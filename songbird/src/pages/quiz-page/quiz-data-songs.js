284f53f750896be4bab24f202014736d30a305e4
evg-zlg <evg.zalogin@gmail.com>
2022-11-07 16:58:33 +0500

    feat: add load info from objects data, update after click next button

--- songbird/src/pages/quiz-page/quiz-data-songs.js
+++ songbird/src/pages/quiz-page/quiz-data-songs.js
@@ -1,15 +1,21 @@
 const songs = [
   {  
-    name: "note-mi",
+    name: "piano",
     answers: ["drum", "guitar", "piano", "accordion", "cello"],
     answer: "piano",
-    mp3: require("./../../assets/sounds/piano-mi.mp3")
+    mp3: require("./../../assets/sounds/piano-mi.mp3"),
+    png: require("./../../assets/images/piano.png"),
+    descript: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio veritatis atque praesentium libero dolores laboriosam, doloribus quisquam commodi iure cumque voluptatum ad quasi, autem sunt veniam ducimus, fugit iste ex.",
+    sounds: [],
   },
   {  
-    name: "note-mi",
+    name: "maracas",
     answers: ["maracas", "guitar", "tambourine", "xylophone", "cello"],
     answer: "maracas",
-    mp3: require("./../../assets/sounds/piano-mi.mp3")
+    mp3: require("./../../assets/sounds/piano-mi.mp3"),
+    png: require("./../../assets/images/maracas.png"),
+    descript: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio veritatis atque praesentium libero dolores laboriosam.",
+    sounds: [],
   },
 ];
 
--- songbird/src/pages/quiz-page/quiz-page.js
+++ songbird/src/pages/quiz-page/quiz-page.js
@@ -7,20 +7,31 @@ const inputs = document.querySelectorAll(".game__input");
 const btnNext = document.querySelector(".game__next");
 const temp = document.querySelector(".game__temp");
 const count = document.querySelector(".game__count");
+const imgPlayer = document.querySelector(".game__image");
 let score = 0;
 let scoreTemp = 0;
-
-let currentQuest = new Object;;
+let currentQuestIndex = 0;
+let currentQuest = new Object;
 
 // on load quiz
 document.addEventListener("DOMContentLoaded", () => {
   updateQuestion();
+  // add click for answers
+  for (let i = 0; i < currentQuest.answers.length; i++) {
+    inputs[i].addEventListener("click", handlerAnswersInput);
+  }  
 });
 
-function choiseQuest() {
-  currentQuest = songs[0];
-}
+// function choiseQuest() {
+//   currentQuest = songs[0];
+// }
+btnNext.addEventListener("click", handlerBtnNext)
 
+function handlerBtnNext() {
+
+  updateQuestion();
+  
+}
 function checkAnswer(answer, obj) {
   if (answer == obj.answer) {
     return true
@@ -37,6 +48,9 @@ function handlerAnswersInput (e) {
     document.querySelector(".info").classList.remove("info--masked");
     score += scoreTemp;
     count.innerText = score;
+    //load quest image
+    imgPlayer.src = currentQuest.png;
+
   } else {
     e.target.parentNode.classList.add("game__answer--is-wrong");
     scoreTemp -= 2;
@@ -45,13 +59,41 @@ function handlerAnswersInput (e) {
 }
 
 function updateQuestion() {
+  
+  //update score
   scoreTemp = 10;
-  choiseQuest();
-  load(currentQuest);
-  for (let i = 0; i < currentQuest.answers.length; i++) {
-    inputs[i].parentNode.childNodes[1].data = currentQuest.answers[i];
-    inputs[i].addEventListener("click", handlerAnswersInput);
-  };
+  currentQuest = songs[currentQuestIndex++];
+  
+  // clear answers block
+  inputs[0].parentNode.parentNode.classList.remove("game__inputs--disabled");
+  inputs.forEach(elem => {
+    elem.parentNode.classList.remove("game__answer--is-right");
+    elem.parentNode.classList.remove("game__answer--is-wrong");
+    elem.checked = false;
+  });
+
+  //disabled button netx
+  btnNext.disabled = true;
+
+  // hidden info
+  document.querySelector(".info").classList.add("info--masked");
+
+  setTimeout(() => {
+    // load audio
+    load(currentQuest);
+
+    // load answers
+    for (let i = 0; i < currentQuest.answers.length; i++) {
+      inputs[i].parentNode.childNodes[1].data = currentQuest.answers[i];
+    };
+    //load quest image by default
+    imgPlayer.src = require("./../../assets/images/collage-main.png");
+    //load info
+    document.querySelector(".info__image").src = currentQuest.png;
+    document.querySelector(".info__text").innerText = currentQuest.descript;
+    
+  }, 300);
+  
 };
 
 
